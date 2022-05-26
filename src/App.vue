0891ca3a54e40d473c77469594ff4950ecfbba5d
kaguyaosu <kaguyaosu3@gmail.com>
2022-05-26 07:01:00 +0300

    fuck

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+    <router-link to="/">Events</router-link> |
+    <router-link to="/about">About</router-link> |
+    <router-link to="/links">Links</router-link>
   </nav>
   <router-view/>
 </template>
@@ -26,5 +27,9 @@ nav a {
 
 nav a.router-link-exact-active {
   color: #42b983;
+}
+
+h4 {
+  font-size: 20px;
 }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,19 +1,24 @@
 import { createRouter, createWebHashHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import EventsList from '../views/EventsList.vue'
+import LinksView from '../views/LinksView'
+import About from '../views/AboutView'
+
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: HomeView
+    name: 'EventsList',
+    component: EventsList
   },
   {
     path: '/about',
     name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    component: About
+  },
+  {
+    path: '/links',
+    name: 'links',
+    component: LinksView
   }
 ]
 
